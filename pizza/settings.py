48726fa82ccbe0d3615fbf0090137fd60d255a6f
Arnaud Buisson <arnaud.buisson@hec.edu>
2020-07-16 12:03:00 +0200

    fixed settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -23,7 +23,7 @@ def get_secret(setting, secrets=secrets):
     try:
         return secrets[setting]
     except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting)
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
